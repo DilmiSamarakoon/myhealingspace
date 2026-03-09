1a2484d73df21dd69366dcf61e470a5c124969bc
Amashi Dilmika <148948817+DilmiSamarakoon@users.noreply.github.com>
2026-03-09 13:14:37 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey:            "AIzaSyCuH7ri961hF6L983W6uu28Bvw8JTEGOxc",
+  authDomain:        "articles-2d6e2.firebaseapp.com",
+  projectId:         "articles-2d6e2",
+  storageBucket:     "articles-2d6e2.firebasestorage.app",
+  messagingSenderId: "450668688360",
+  appId:             "1:450668688360:web:36312902989aca92b1ea5d"
+};
